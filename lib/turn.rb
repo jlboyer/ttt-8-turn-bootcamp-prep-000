18c35949d4aea01c07eb96d7560ea453b8b83c7e
mathematical-algorithm-3478 <jlboyer1@gmail.com>
2019-05-16 19:35:44 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -20,7 +20,7 @@ end
 
 
 def valid_move?(board, index)
-  if index.between?(0, 8) && position_taken?(board, index) == false
+  if index =~ /[^0-8]/  && position_taken?(board, index) == false
     true
   else
     false
