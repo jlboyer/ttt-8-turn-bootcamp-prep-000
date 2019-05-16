c7e087770efb3b449d13301135fa44f379a834ae
mathematical-algorithm-3478 <jlboyer1@gmail.com>
2019-05-16 19:30:49 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -17,3 +17,20 @@ end
 def move(board,index,character = "X")
   board[index] = character
 end
+
+
+def valid_move?(board, index)
+  if index.between?(0, 8) && position_taken?(board, index) == false
+    true
+  else
+    false
+  end
+end
+
+ def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    false
+  else
+    true
+  end
+end
